866f4eeb9c3d2b71f2bbeb39c108d67318b83120
Cyan512 <182935@unsaac.edu.pe>
2026-04-27 09:09:35 -0500

    feat(gallery): add GalleryList component with type definitions

--- src/app/[locale]/(root)/gallery/page.tsx
+++ src/app/[locale]/(root)/gallery/page.tsx
@@ -1,17 +1,47 @@
+import { getContentPage } from '@/src/api/strapi/getContentPage';
 import GalleryList from '@/src/components/pages/gallery/GalleryList';
 import SharedHero from '@/src/components/pages/SharedHero';
-import { useTranslations } from 'next-intl';
+import { GalleryBlocks, GalleryContent, GalleryListEntity } from '@/src/types/pages/Gallery';
+import { getTranslations, setRequestLocale } from 'next-intl/server';
 
-export default function Gallery() {
-  const t = useTranslations('gallery.hero');
-  return (
-    <>
-      <SharedHero
-        title={t('title')}
-        description={t('description')}
-        backgroundImage="/bg-about-2.jpg"
-      />
-      <GalleryList />
-    </>
-  );
+type Props = {
+  params: Promise<{ locale: string }>;
+};
+
+type Translator = Awaited<ReturnType<typeof getTranslations>>;
+
+function renderComponent(component: GalleryBlocks, index: number, t: Translator) {
+  const key = `${component.id}-${index}`;
+
+  switch (component.__component) {
+    case 'shared.section-hero':
+      return (
+        <SharedHero
+          key={key}
+          title={t('title')}
+          description={t('description')}
+          backgroundImage="/bg-about-2.jpg"
+        />
+      );
+
+    case 'gallery.gallery-list':
+      return <GalleryList key={key} data={component as GalleryListEntity} />;
+
+    default:
+      return null;
+  }
+}
+
+export default async function Gallery({ params }: Props) {
+  const endpoint = 'gallery-page';
+  const { locale } = await params;
+
+  setRequestLocale(locale);
+
+  const res = await getContentPage<GalleryContent>(endpoint, locale);
+  const content = res.data.content;
+
+  const t = await getTranslations('gallery.hero');
+
+  return <>{content.map((component, index) => renderComponent(component, index, t))}</>;
 }
---
+++ src/types/pages/Gallery.ts
@@ -0,0 +1,14 @@
+import { Img, SharedSectionAbout, SharedSectionHero } from '@/src/types/Shared';
+import { StrapiEntity } from '@/src/types/Strapi';
+
+export interface GalleryListEntity {
+  __component: 'gallery.gallery-list';
+  id: number;
+  images: Img[];
+}
+
+export type GalleryBlocks = SharedSectionHero | GalleryListEntity;
+
+export type GalleryContent = StrapiEntity<{
+  content: GalleryBlocks[];
+}>;
